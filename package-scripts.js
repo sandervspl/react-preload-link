734bbe34f63d498492fb642d8a911c938ab8b53c
Sander Vispoel <sandervispoel@hotmail.com>
2018-01-21 18:00:58 +0100

    Update Rollup config and build script

--- package-scripts.js
+++ package-scripts.js
@@ -5,31 +5,26 @@ const rimraf = npsUtils.rimraf;
 const concurrent = npsUtils.concurrent;
 
 module.exports = {
-	scripts: {
-		build: {
-			description: 'clean dist directory and run all builds',
-			default: series(
-				rimraf('dist'),
-				rimraf('lib'),
-				concurrent.nps('build.css', 'build.cssmin'),
-				concurrent.nps('build.rollup', 'build.babel')
-			),
-			rollup: 'rollup --config',
-			babel: 'babel src -d lib',
-			css: 'lessc less/default.less dist/react-select.css',
-			cssmin: 'lessc --clean-css less/default.less dist/react-select.min.css',
-			standalone: series(
-				'cp examples/src/standalone.html examples/dist/standalone.html',
-				'lessc examples/src/example.less examples/dist/example.css'
-			),
-		},
-		publish: {
-			default: series(
-				rimraf('examples/dist'),
-				'webpack --progress -p',
-				'cp examples/src/.gitignore examples/dist/.gitignore',
-				'gh-pages -d examples/dist'
-			),
-		},
-	},
+    scripts: {
+        build: {
+            description: 'clean dist directory and run all builds',
+            default: series(
+                rimraf('dist'),
+                rimraf('lib'),
+                // concurrent.nps('build.css', 'build.cssmin'),
+                concurrent.nps('build.rollup', 'build.babel'),
+            ),
+            rollup: 'rollup --config',
+            babel: 'babel src -d lib',
+            standalone: 'cp examples/src/standalone.html examples/dist/standalone.html',
+        },
+        publish: {
+            default: series(
+                rimraf('examples/dist'),
+                'webpack --progress -p',
+                'cp examples/src/.gitignore examples/dist/.gitignore',
+                'gh-pages -d examples/dist',
+            ),
+        },
+    },
 };
---
+++ src/index.umd.js
@@ -0,0 +1,6 @@
+import { default as PreloadLink, PreloadLinkInit, PRELOAD_FAIL } from './PreloadLink';
+
+PreloadLink.PRELOAD_FAIL = PRELOAD_FAIL;
+PreloadLink.PreloadLinkInit = PreloadLinkInit;
+
+export default PreloadLink;
