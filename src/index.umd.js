4731669a15c040943246478bd7c19206ebf49c48
Sander Vispoel <sandervispoel@hotmail.com>
2018-01-24 18:54:07 +0100

    Update umd index

--- src/index.umd.js
+++ src/index.umd.js
@@ -1,6 +1,6 @@
-import { default as PreloadLink, PreloadLinkInit, PRELOAD_FAIL } from './PreloadLink';
+import { default as PreloadLink, preloadLinkConfig, PRELOAD_FAIL } from './PreloadLink';
 
 PreloadLink.PRELOAD_FAIL = PRELOAD_FAIL;
-PreloadLink.PreloadLinkInit = PreloadLinkInit;
+PreloadLink.preloadLinkConfig = preloadLinkConfig;
 
 export default PreloadLink;
