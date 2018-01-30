a1742d89e6117d4388c7ee0d92b55e2e604e2f29
Sander Vispoel <contact@sandervispoel.com>
2018-01-30 13:07:27 +0100

    Add route change test

--- test/PreloadLink.react.test.js
+++ test/PreloadLink.react.test.js
@@ -19,108 +19,130 @@ describe('App', () => {
     });
 });
 
-describe('<PreloadLink to="/" />', () => {
-    let component;
-    let instance;
-
-    beforeEach(() => {
-        component = mount(<Router>
-            <PreloadLink to="/" />
-        </Router>);
-    });
-
-    const getPreloadLink = () => component.find(PreloadLink);
-
-    it('Renders a <PreloadLink> component', () => {
-        instance = getPreloadLink();
-        expect(instance.length).toEqual(1);
-    });
-
-    it('"to" is "/"', () => {
-        instance = getPreloadLink();
-        expect(instance.prop('to')).toEqual('/');
-    });
-});
-
-describe('<PreloadLink to="/" noInterrupt />', () => {
-    let component;
-    let instance;
-
-    beforeEach(() => {
-        component = shallow(<Router>
-            <PreloadLink to="/" noInterrupt />
-        </Router>);
-    });
-
-    const getPreloadLink = () => component.find(PreloadLink);
-
-    it('"noInterrupt" is true', () => {
-        instance = getPreloadLink();
-        expect(instance.prop('noInterrupt')).toEqual(true);
-    });
-});
-
-describe('PreloadLink with hook override props', () => {
+describe('<PreloadLink>', () => {
     let component;
     let instance;
 
     const getPreloadLink = () => component.find(PreloadLink);
 
-    beforeEach(() => {
-        component = shallow(<Router>
-            <PreloadLink to="/" noInterrupt />
-        </Router>);
+    describe('[to="/"]', () => {
+        beforeEach(() => {
+            component = mount(
+                <Router>
+                    <PreloadLink to="/" />
+                </Router>
+            );
+
+            instance = getPreloadLink();
+        });
+
+        it('Renders a <PreloadLink> component', () => {
+            expect(instance.length).toEqual(1);
+        });
+
+        it('"to" is "/"', () => {
+            expect(instance.prop('to')).toEqual('/');
+        });
     });
 
-    it('<PreloadLink onLoading={fn} /> has prop', () => {
-        instance = getPreloadLink();
-        expect(instance.prop('onLoading')).not.toEqual(null);
-    });
+    const url = '/page2';
+    describe(`[to="${url}"]`, () => {
+        beforeEach(() => {
+            component = mount(
+                <Router>
+                    <PreloadLink to={url} />
+                </Router>
+            );
 
-    it('<PreloadLink onSuccess={fn} /> has prop', () => {
-        instance = getPreloadLink();
-        expect(instance.prop('onSuccess')).not.toEqual(null);
-    });
+            instance = getPreloadLink();
+        });
 
-    it('<PreloadLink onFail={fn} /> has prop', () => {
-        instance = getPreloadLink();
-        expect(instance.prop('onFail')).not.toEqual(null);
+        it(`Changes route to "${url}" after click`, () => {
+            instance.simulate('click');
+            expect(component.instance().history.location.pathname).toEqual(url);
+        });
     });
 });
 
-describe('Configuration', () => {
-    const Class = PreloadLink.WrappedComponent;
-
-    it('Has a function for "onLoading"', () => {
-        pll.configure({
-            onLoading: () => {},
-        });
-
-        expect(Class.onLoading).not.toEqual(undefined);
-        expect(Class.onSuccess).toEqual(undefined);
-        expect(Class.onFail).toEqual(undefined);
-    });
-
-    it('Has a function for "onLoading", "onSuccess"', () => {
-        pll.configure({
-            onLoading: () => {},
-            onSuccess: () => {},
-        });
-
-        expect(Class.onLoading).not.toEqual(undefined);
-        expect(Class.onSuccess).not.toEqual(undefined);
-        expect(Class.onFail).toEqual(undefined);
-    });
-
-    it('Has a function for "onLoading", "onSuccess", "onFail"', () => {
-        pll.configure({
-            onLoading: () => {},
-            onSuccess: () => {},
-            onFail: () => {},
-        });
-
-        expect(Class.onLoading).not.toEqual(undefined);
-        expect(Class.onSuccess).not.toEqual(undefined);
-        expect(Class.onFail).not.toEqual(undefined);
-    });
-});
+// describe('<PreloadLink to="/" noInterrupt />', () => {
+//     let component;
+//     let instance;
+//
+//     beforeEach(() => {
+//         component = shallow(<Router>
+//             <PreloadLink to="/" noInterrupt />
+//         </Router>);
+//     });
+//
+//     const getPreloadLink = () => component.find(PreloadLink);
+//
+//     it('"noInterrupt" is true', () => {
+//         instance = getPreloadLink();
+//         expect(instance.prop('noInterrupt')).toEqual(true);
+//     });
+// });
+//
+// describe('PreloadLink with hook override props', () => {
+//     let component;
+//     let instance;
+//
+//     const getPreloadLink = () => component.find(PreloadLink);
+//
+//     beforeEach(() => {
+//         component = shallow(<Router>
+//             <PreloadLink to="/" noInterrupt />
+//         </Router>);
+//     });
+//
+//     it('<PreloadLink onLoading={fn} /> has prop', () => {
+//         instance = getPreloadLink();
+//         expect(instance.prop('onLoading')).not.toEqual(null);
+//     });
+//
+//     it('<PreloadLink onSuccess={fn} /> has prop', () => {
+//         instance = getPreloadLink();
+//         expect(instance.prop('onSuccess')).not.toEqual(null);
+//     });
+//
+//     it('<PreloadLink onFail={fn} /> has prop', () => {
+//         instance = getPreloadLink();
+//         expect(instance.prop('onFail')).not.toEqual(null);
+//     });
+// });
+//
+// describe('Configuration', () => {
+//     const Class = PreloadLink.WrappedComponent;
+//
+//     it('Has a function for "onLoading"', () => {
+//         pll.configure({
+//             onLoading: () => {},
+//         });
+//
+//         expect(Class.onLoading).not.toEqual(undefined);
+//         expect(Class.onSuccess).toEqual(undefined);
+//         expect(Class.onFail).toEqual(undefined);
+//     });
+//
+//     it('Has a function for "onLoading", "onSuccess"', () => {
+//         pll.configure({
+//             onLoading: () => {},
+//             onSuccess: () => {},
+//         });
+//
+//         expect(Class.onLoading).not.toEqual(undefined);
+//         expect(Class.onSuccess).not.toEqual(undefined);
+//         expect(Class.onFail).toEqual(undefined);
+//     });
+//
+//     it('Has a function for "onLoading", "onSuccess", "onFail"', () => {
+//         pll.configure({
+//             onLoading: () => {},
+//             onSuccess: () => {},
+//             onFail: () => {},
+//         });
+//
+//         expect(Class.onLoading).not.toEqual(undefined);
+//         expect(Class.onSuccess).not.toEqual(undefined);
+//         expect(Class.onFail).not.toEqual(undefined);
+//     });
+// });
