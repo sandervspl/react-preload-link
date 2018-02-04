50e4b7d2dea407aa93f226a290bccffcb4b4768a
Sander Vispoel <sandervispoel@hotmail.com>
2018-02-04 12:33:11 +0100

    Wrap definition file in a module

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,32 @@
-import * as React from 'react';
+declare module 'react-preload-link' {
+    import * as React from 'react';
 
-export interface PreloadLinkConfigOptions {
-    onLoading?: () => any;
-    onSuccess?: () => any;
-    onFail?: () => any;
-    onNavigate?: () => any;
+    export interface PreloadLinkConfigOptions {
+        onLoading?: () => any;
+        onSuccess?: () => any;
+        onFail?: () => any;
+        onNavigate?: () => any;
+    }
+
+    export type PreloadLinkConfig = (options: PreloadLinkConfigOptions) => void;
+
+    export type PreloadLinkLifecycleHook = (defaultHook: () => void) => any;
+
+    type PromiseFn = () => Promise<any>;
+
+    export interface PreloadLinkProps {
+        to: string,
+        load?: PromiseFn | PromiseFn[],
+        onLoading?: PreloadLinkLifecycleHook,
+        onSuccess?: PreloadLinkLifecycleHook,
+        onFail?: PreloadLinkLifecycleHook,
+        onNavigate?: PreloadLinkLifecycleHook,
+        noInterrupt?: boolean,
+    }
+
+    export default class PreloadLink extends React.Component<PreloadLinkProps, any> {}
+
+    export const configure: PreloadLinkConfig;
+
+    export const PRELOAD_FAIL: string;
 }
-
-export type PreloadLinkConfig = (options: PreloadLinkConfigOptions) => void;
-
-export type PreloadLinkLifecycleHook = (defaultHook: () => void) => any;
-
-type PromiseFn = () => Promise<any>;
-export interface PreloadLinkProps {
-    to: string,
-    load?: PromiseFn | PromiseFn[],
-    onLoading?: PreloadLinkLifecycleHook,
-    onSuccess?: PreloadLinkLifecycleHook,
-    onFail?: PreloadLinkLifecycleHook,
-    onNavigate?: PreloadLinkLifecycleHook,
-    noInterrupt?: boolean,
-}
-export default class PreloadLink extends React.Component<PreloadLinkProps, any> {}
-
-export const configure: PreloadLinkConfig;
-
-export const PRELOAD_FAIL: string;
